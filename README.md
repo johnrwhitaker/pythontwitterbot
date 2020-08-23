4fc63d83bf072489b6c6bf24ca5bf1cf18794077
John Whitaker <jwhitaker23@gmail.com>
2020-08-22 21:40:29 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+You will need to create a file named credentials.py with your twitter app crednetial settings
---
+++ config.py
@@ -0,0 +1,11 @@
+# The hashtage that the bot will search for
+QUERY = '#rhcsa8'
+
+# Set to true if you'd like to like the tweets
+LIKE = True
+
+# Set to ttrue if you'd like to follow the user
+FOLLOW = True
+
+# Sleep time in seconds to prevent flooding the API
+SLEEP_TIME = 300
